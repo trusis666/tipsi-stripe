4a7e23ce07f17bce283a8f3dd4848d4e64468717
Arturs Škaraveckis <35992243+trusis666@users.noreply.github.com>
2020-06-04 11:49:40 +0300

    Update TPSCardField.m
    temp fix for ios crash

--- ios/TPSStripe/TPSCardField.m
+++ ios/TPSStripe/TPSCardField.m
@@ -18,9 +18,9 @@
 
 }
 
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:self.window];
-}
+// - (void)dealloc {
+//     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:self.window];
+// }
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
@@ -29,11 +29,11 @@
         _paymentCardTextField.delegate = self;
         [self addSubview:_paymentCardTextField];
         self.backgroundColor = [UIColor clearColor];
-        [[NSNotificationCenter defaultCenter]
-            addObserver:self
-               selector:@selector(keyboardWillShow:)
-                   name:UIKeyboardWillShowNotification
-                 object:self.window];
+//         [[NSNotificationCenter defaultCenter]
+//             addObserver:self
+//                selector:@selector(keyboardWillShow:)
+//                    name:UIKeyboardWillShowNotification
+//                  object:self.window];
     }
     return self;
 }
